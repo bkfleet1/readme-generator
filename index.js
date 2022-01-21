cce995c660d8b048de917883cbf67de750f5d4d6
bkfleet1 <bkfleet1@gmail.com>
2022-01-20 20:39:29 -0600

    project questions working. resolved issue with project languages

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 // TODO: Include packages needed for this application
-const writeToFile = require('fs');
+// const writeToFile = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+// const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+// Introduction, optional sections, and project details questions
 const projectDetails = detailsAnswers => {
+    console.log(`
+===============================
+Welcome to the Readme Generator
+===============================
+
+Readme Genereator is a command-line application that dynamically generates a project README.md file based on your responses to a series of questions(e.g, project name, title, repository url, deployed url, developer names, install instructions, usage, licensing, and credits).
+
+There are also optional sections, such as Badges, Features, Contributor Covenants, and Testing Instructions.
+`);
+
+
+
     return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'optionalConfirm',
+            message: 'Do you want to include any optional sections in your README.md (e.g.,Badges, Features, Contributor Covenants, or Testing Instructions.)?'
+        },
+        {
+            type: 'checkbox',
+            name: 'optional',
+            message: 'Which optional sections do you want to include in your README.md? (Check all that apply)',
+            choices: ['Badges', 'Features', 'Contributor Covenants', 'Testing Instructions'],
+            when: ({ optionalConfirm }) => {
+                if (optionalConfirm) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        },
         {
             type: 'input',
             name: 'title',
-            message: '(Required) What is the name of the project?',
-            validate: titleInput => {
-                if (titleInput) {
+            message: 'What is the name of the project? (Required)',
+            validate: title => {
+                if (title) {
                     return true;
                 } else {
                     console.log('A project name is required. Please enter the name of the project.');
@@ -22,9 +53,9 @@ const projectDetails = detailsAnswers => {
         {
             type: 'input',
             name: 'description',
-            message: '(Required) Please provide a short description of your project. NOTE: A description should include (1. Why was this project created? (2. What problem does it solve? (3. How does it solve the problem?',
-            validate: descriptionInput => {
-                if (descriptionInput) {
+            message: 'Please provide a short description of your project. (Required)',
+            validate: description => {
+                if (description) {
                     return true;
                 } else {
                     console.log('A project description is required. Please provide a short description of your project.');
@@ -35,9 +66,9 @@ const projectDetails = detailsAnswers => {
         {
             type: 'input',
             name: 'repoLink',
-            message: '(Required) Enter the url to the project Github repository.',
-            validate: repoLinkInput => {
-                if (repoLinkInput) {
+            message: 'Enter the url to the project Github repository. (Required)',
+            validate: repoLink => {
+                if (repoLink) {
                     return true;
                 } else {
                     console.log('Please enter the url to the project Github repository.');
@@ -48,9 +79,9 @@ const projectDetails = detailsAnswers => {
         {
             type: 'input',
             name: 'deployedLink',
-            message: '(Required) Enter the url to the deployed project on Github.',
-            validate: deployedLinkInput => {
-                if (deployedLinkInput) {
+            message: 'Enter the url to the deployed project on Github. (Required)',
+            validate: deployedLink => {
+                if (deployedLink) {
                     return true;
                 } else {
                     console.log('Please enter the url to the deployed project on Github');
@@ -62,30 +93,74 @@ const projectDetails = detailsAnswers => {
 };
 
 const projectAuthors = authorsAnswers => {
-    if (!authorsAnswers.developerInput) {
-        authorsAnswers.developerInput = [];
-    }
+    if (!authorsAnswers.developer) {
+        authorsAnswers.developer = [];
+      }
     return inquirer.prompt([
         {
             type: 'input',
             name: 'developer',
-            message: `(Required) Enter name of the project's developer.`,
-            validate: developerInput => {
-                if (developerInput) {
-                    return true;
+            message: `Enter name of the project's developer. (Required)`,
+            validate: developer => {
+                if (developer) {
+                    return true
                 } else {
                     console.log(`Please enter name of the project's developer.`);
+                    return false
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'developers',
+            message: `Are there other developers that you'd like to cite as contributors to the project?`,
+            default: false
+        }
+    ])
+    .then(coders => {
+        authorsAnswers.developer.push(coders);
+        if (coders.developers) {
+            return projectAuthors(authorsAnswers);
+        } else {
+            // console.log(authorsAnswers.developer);
+            return authorsAnswers.developer;
+        }
+    })
+};
+
+
+
+//if we wanted to add a Other choice and allow a user to enter languages not listed?
+const projectResources = resourcesAnswers => {
+    if (!resourcesAnswers) {
+        resourcesAnswers = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What development languages and libraries were used to create this project? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node', 'Inquirer'],
+            validate: languages => {
+                if (languages) {
+                    return true;
+                } else {
+                    console.log('Please select the development languages and libraries used to create this project!');
                     return false;
                 }
             }
         },
         {
+            type: 'confirm',
+            name: 'languagesConfirm',
+            message: `Are there development languages and libraries that were used in this project?`
+        },
+        {
             type: 'input',
-            name: 'developers',
-            message: `Are there other developers that you'd like to cite as contributors to the project?`,
-            default: false,
-            when: ({ developers }) => {
-                if (developers) {
+            name: 'languagesOther',
+            message: `Please enter other development languages and libraries used in this project not previously cited, separating each with a comma.`,
+            when: ({ languagesConfirm }) => {
+                if (languagesConfirm) {
                     return true;
                 } else {
                     return false;
@@ -93,37 +168,10 @@ const projectAuthors = authorsAnswers => {
             }
         }
     ])
-        .then(authorsAnswers => {
-            authorsAnswers.developerInput.push(authorsAnswers);
-            if (authorsAnswers.developers) {
-                return projectAuthors(authorsAnswers);
-            } else {
-                return authorsAnswers;
-            }
-        });
-    };
+};
 
 
 
-//if we wanted to add a Other choice and allow a user to enter languages not listed?
-// {
-//     type: 'checkbox',
-//         name: 'languages',
-//             message: 'What development languages and libraries were used to create this project? (Check all that apply)',
-//                 choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node', 'Inquirer'],
-//                     validate: languagesCheckbox => {
-//                         if (languagesCheckbox) {
-//                             console.log(languagesCheckbox);
-//                             return true;
-//                         } else {
-//                             console.log('Please select the development languages and libraries used to create this project!');
-//                             return false;
-//                         }
-//                     }
-// }
-
-
- 
 // // TODO: Create a function to write README file
 // function  writeToFile = fileContent => {
 //     return new Promise((resolve, reject) => {
@@ -146,17 +194,20 @@ const projectAuthors = authorsAnswers => {
 
 // TODO: Create a function to initialize app
 // function init() {
-//     npm install inquirer
+//     $ ('npm install inquirer')
 // }
 
 // Function call to initialize app
-init()
-    .then(projectDetails)
+// init()
+// .then(projectDetails)
+projectDetails()
     .then(projectAuthors)
     .then(projectResources)
-    .then(data => {
-        return generateMarkdown(data)
-    })
-    .then(writeToFile => {
-        return writeFile(writeToFile)
-    })
+
+
+            // .then(data => {
+            //     return generateMarkdown(data)
+            // })
+            // .then(writeToFile => {
+            //     return writeFile(writeToFile)
+            // })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,9 +6,15 @@ function renderLicenseBadge(license) {}
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
+https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+
+function renderContributor() {
+  https://www.contributor-covenant.org/version/2/1/code_of_conduct/code_of_conduct.md
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
