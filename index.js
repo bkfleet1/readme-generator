ccf291841296a7f2d5ffcee261bb7c122928f721
bkfleet1 <bkfleet1@gmail.com>
2022-01-23 17:44:23 -0600

    combined the Inquirer responses into a single array

--- index.js
+++ index.js
@@ -2,42 +2,23 @@
 const writeToFile = require('fs');
 const inquirer = require('inquirer');
 const fetch = require("node-fetch");
-// const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 // Introduction, optional sections, and project details questions
 const projectDetails = detailsAnswers => {
+    if (!detailsAnswers) {
+        detailsAnswers = [];
+    }
     console.log(`
 ===============================
 Welcome to the Readme Generator
 ===============================
 
-Readme Genereator is a command-line application that dynamically generates a project README.md file based on your responses to a series of questions(e.g, project name, title, repository url, deployed url, developer names, install instructions, usage, licensing, and credits).
+Readme Genereator is a command-line application that dynamically generates a project README.md file based on your responses to a series of questions(e.g, Project Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions).
 
-There are also optional sections, such as Badges, Features, Contributor Covenants, and Testing Instructions.
 `);
-
-
-
     return inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'optionalConfirm',
-            message: 'Do you want to include any optional sections in your README.md (e.g.,Badges, Features, Contributor Covenants, or Testing Instructions.)?'
-        },
-        {
-            type: 'checkbox',
-            name: 'optional',
-            message: 'Which optional sections do you want to include in your README.md? (Check all that apply)',
-            choices: ['Badges', 'Features', 'Contributor Covenants', 'Testing Instructions'],
-            when: ({ optionalConfirm }) => {
-                if (optionalConfirm) {
-                    return true
-                } else {
-                    return false
-                }
-            }
-        },
         {
             type: 'input',
             name: 'title',
@@ -91,12 +72,17 @@ There are also optional sections, such as Badges, Features, Contributor Covenant
             }
         }
     ])
+        .then(data => {
+            detailsAnswers.push(data)
+            console.log(detailsAnswers)
+            return detailsAnswers
+        })
 };
 
 const projectAuthors = authorsAnswers => {
-    if (!authorsAnswers.developer) {
-        authorsAnswers.developer = [];
-    }
+    // if (!authorsAnswers) {
+    //     authorsAnswers = [];
+    // }
     return inquirer.prompt([
         {
             type: 'input',
@@ -112,19 +98,32 @@ const projectAuthors = authorsAnswers => {
             }
         },
         {
+            type: 'input',
+            name: 'githubId',
+            message: `What is the developer's Github Id? (Required)`,
+            validate: githubId => {
+                if (githubId) {
+                    return true
+                } else {
+                    console.log(`A Github Id is required. Please enter the developer's Github Id`);
+                    return false
+                }
+            }
+        },
+        {
             type: 'confirm',
             name: 'developers',
             message: `Are there other developers that you'd like to cite as contributors to the project?`,
             default: false
         }
     ])
-        .then(coders => {
-            authorsAnswers.developer.push(coders);
-            if (coders.developers) {
+        .then(data => {
+            authorsAnswers.push(data);
+            if (data.developers) {
                 return projectAuthors(authorsAnswers);
             } else {
-                // console.log(authorsAnswers.developer);
-                return authorsAnswers.developer;
+                console.log(authorsAnswers);
+                return authorsAnswers;
             }
         })
 };
@@ -132,10 +131,7 @@ const projectAuthors = authorsAnswers => {
 
 
 //if we wanted to add a Other choice and allow a user to enter languages not listed?
-const projectResources = resourcesAnswers => {
-    if (!resourcesAnswers) {
-        resourcesAnswers = [];
-    }
+const projectResources = async resourcesAnswers => {
     return inquirer.prompt([
         {
             type: 'checkbox',
@@ -167,20 +163,87 @@ const projectResources = resourcesAnswers => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'list',
+            name: 'licenseX',
+            message: 'Select a development license applies to this project? (Check all that apply)',
+            choices: await licenses(),
+            validate: licenseX => {
+                if (licenseX) {
+                    return true;
+                } else {
+                    console.log('Please select the development license that applies to this project!');
+                    return false;
+                }
+            }
         }
+
     ])
+        .then(data => {
+            resourcesAnswers.push(data);
+            console.log(resourcesAnswers);
+            //   return resourcesAnswers
+            licenseDesc(resourcesAnswers);
+        })
 };
 
-const licenses = () => {
-    fetch("https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json")
-    .then((response) => response.json())
-    .then((data) => {const licenseData = data.licenses;
-                for (let i = 0; i < licenseData.length; i++) {
-                    const licenseId = licenseData[i].licenseId
-                    const licenseName = licenseData[i].name
-                    console.log(licenseId, licenseName)}})
-    .catch((err) => console.log(err))
-}
+
+const licenses = async () => {
+    const licenseArr = []
+ /
+    fetch("https://api.github.com/licenses")
+        .then((response) => response.json())
+        .then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                const licenseName = data[i].name
+                const licenseUrl = data[i].url
+                // localStorage.setItem(licenseName,licenseUrl)
+                licenseArr.push(licenseName)
+                // licenseArr2.push(licenseName,licenseUrl)
+            }
+        })
+        .catch((err) => console.log(err))
+    return licenseArr
+};
+
+// const licenseDesc = (resourcesAnswers,licenseArr2) => {
+//     console.log(licenseArr2);
+//     console.log(resourcesAnswers);
+    // for (let i = 0; i < resourcesAnswers.length; i++) {
+    //     const licenseX = resourcesAnswers[i].licenseX;
+    //     console.log(licenseX);
+    //     {for (let index = 0; index < licenseArr2.length; index++) {
+    //         const l_name = licenseArr2[index].licenseName;
+    //         const l_url = licenseArr2[index].licenseUrl;
+    //         if (l_name === licenseX) {
+    //             console.log(l_url)} 
+    //         }
+            
+    //     }
+    // }
+    // } 
+
+const licenseDetails = (resourcesAnswers) => {
+    console.log(resourcesAnswers);
+
+    // fetch(`https://api.github.com/licenses/${licenseX}`)
+    //     .then((response) => response.json())
+    //     .then(data =>{
+    //         licenseDesc.push(data);
+    //         console.log(licenseDesc);
+    //         return licenseDesc;
+    //     })
+    //     // .then((data) => ({
+    //     //     licenseKey: data.key,
+    //     //     licenseDesc: data.description
+    //     //                  }))
+    //     .catch((err) => console.log(err))
+};
+
+
+
+
 
 
 // // TODO: Create a function to write README file
@@ -210,11 +273,17 @@ const licenses = () => {
 
 // Function call to initialize app
 // init()
-// .then(projectDetails)
+
 projectDetails()
     .then(projectAuthors)
     .then(projectResources)
     .then(licenses)
+    .then(licenseDetails)
+    // .then(generateMarkdown)
+
+    // .then(data=>generateMarkdown({
+    //     licenseDetails: licenseDetails(data.licenseX), ...data
+    // }))
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,41 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json
+// https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-function renderContributor() {
-  https://www.contributor-covenant.org/version/2/1/code_of_conduct/code_of_conduct.md
-}
+// function renderContributor() {
+//   https://www.contributor-covenant.org/version/2/1/code_of_conduct/code_of_conduct.md
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+//   return `# [${data.title}](#title)
 
-`;
+//   ## [${data.description}](#description)
+
+//   ## Table of Contents
+//   > * [Title](#title)
+//   > * [Description](#description)
+//   > * [Installation](#installation)
+//   > * [Usage](#usage)
+//   > * [License](#license)
+//   > * [Contributing](#contributing)
+//   > * [Tests](#tests)
+//   > * [Questions](#questions)
+
+
+
+// `;
 }
 
 module.exports = generateMarkdown;
